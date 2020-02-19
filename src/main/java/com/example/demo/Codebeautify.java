eaaafdf7e137877242c6106406498f211b3ed6a4
Jaszczyk <47436470+Jaszczyk@users.noreply.github.com>
2020-02-19 18:42:05 +0100

    nie działa sciaganie kasy z nbp

---
+++ src/main/java/com/example/demo/Codebeautify.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Codebeautify {
+
+    private MainCodebeautify main;
+
+    public MainCodebeautify getMain() {
+        return main;
+    }
+
+    public void setMain(MainCodebeautify main) {
+        this.main = main;
+    }
+
+
+
+}
